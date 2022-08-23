78753f0d1d38ea49959da73170bec7e41530240c
Alper Aktaş <aktasalper0708@gmail.com>
2022-08-23 11:07:11 +0300

    Commit 1

---
+++ src/demoProjects/rentACar/Application/Features/Brands/Commands/Create/CreateBrandCommand.cs
@@ -0,0 +1,51 @@
+﻿using Application.Features.Brands.Dtos;
+using Application.Services.Repositories;
+using AutoMapper;
+using Domain.Entities;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Brands.Commands.Create
+{
+    public class CreateBrandCommand : IRequest<CreatedBrandDto>
+    {
+        public string Name { get; set; }
+
+        public class CreateBrandCommandHandler : IRequestHandler<CreateBrandCommand, CreatedBrandDto>
+        {
+            private readonly IBrandRepository _brandRepository;
+            private readonly IMapper _mapper;
+
+            public CreateBrandCommandHandler(IBrandRepository brandRepository, IMapper mapper)
+            {
+                _brandRepository = brandRepository;
+                _mapper = mapper;
+            }
+
+            public async Task<CreatedBrandDto> Handle(CreateBrandCommand request, CancellationToken cancellationToken)
+            {
+                Brand mappedBrand = _mapper.Map<Brand>(request);
+                Brand CreatedBrand= await _brandRepository.AddAsync(mappedBrand);
+                CreatedBrandDto createdBrandDto = _mapper.Map<CreatedBrandDto>(CreatedBrand);
+                // CreatedBrandDto createdBrandDto = new CreatedBrandDt();
+                // createdBrandDto.Name=mappedBrand.Name;
+                //Bu şekilde maplerde farklı isimlendirmedeki fieldlar eşleştirilebilir
+                return createdBrandDto;
+
+                
+
+            }
+         //dto=>id,name
+         //brand=>id,name, ileride belki x
+            
+
+
+        }
+    }
+}
+
--- src/demoProjects/rentACar/WebAPI/Program.cs
+++ src/demoProjects/rentACar/WebAPI/Program.cs
@@ -1,3 +1,4 @@
+using Application;
 using Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -5,11 +6,11 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-//builder.Services.AddApplicationServices();
+builder.Services.AddApplicationServices();
 //builder.Services.AddSecurityServices();
 builder.Services.AddPersistenceServices(builder.Configuration);
 //builder.Services.AddInfrastructureServices();
-//builder.Services.AddHttpContextAccessor();
+builder.Services.AddHttpContextAccessor();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -23,7 +24,6 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
 app.UseAuthorization();
 
 app.MapControllers();
